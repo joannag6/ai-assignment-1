2f7a4bb9035bcfdaeedb1f5a8bcc48668c22b3da
Joanna <joannag6@gmail.com>
2018-03-17 13:38:51 +1100

    Store board pieces in sets alone, no need 2d array anymore

--- calculateMoves.py
+++ calculateMoves.py
@@ -1,12 +1,18 @@
-board = [] # 2D array accessible by coordinates
+BOARD_SIZE = 8
+blackPieces = set() 
+whitePieces = set()
 whiteMoves = 0
 blackMoves = 0
 
+def corner(i, j):
+    corner_coords = {0, BOARD_SIZE-1}
+    return i in corner_coords and j in corner_coords
+
 def isEmpty(i, j):
-    return board[i][j] == '-'
+    return (i, j) not in blackPieces and (i, j) not in whitePieces and not corner(i, j)
 
 def withinBounds(i, j):
-    return (0 <= i <= len(board)-1) and (0 <= j <= len(board)-1)
+    return (0 <= i < BOARD_SIZE) and (0 <= j < BOARD_SIZE)
 
 def canJumpRight(i, j):
     return (i,j+2) if withinBounds(i,j+2) and isEmpty(i,j+2) else None
@@ -24,35 +30,39 @@ def canGoRight(i, j):
     return (i, j+1) if withinBounds(i,j+1) and isEmpty(i,j+1) else canJumpRight(i, j)
 
 def canGoLeft(i, j):
-     return (i,j-1) if withinBounds(i,j-1) and isEmpty(i,j-1) else canJumpLeft(i, j)
+    return (i,j-1) if withinBounds(i,j-1) and isEmpty(i,j-1) else canJumpLeft(i, j)
 
 def canGoUp(i, j):
-     return (i-1,j) if withinBounds(i-1,j) and isEmpty(i-1,j) else canJumpUp(i, j)
+    return (i-1,j) if withinBounds(i-1,j) and isEmpty(i-1,j) else canJumpUp(i, j)
 
 def canGoDown(i, j):
-     return (i+1,j) if withinBounds(i+1,j) and isEmpty(i+1,j) else canJumpDown(i, j)
+    return (i+1,j) if withinBounds(i+1,j) and isEmpty(i+1,j) else canJumpDown(i, j)
 
-def calcMoves(i, j): # TODO(return list of possible moves)
+def calcMoves(coord): # TODO(return list of possible moves)
+    i, j = coord
     return [x for x in [canGoRight(i, j), canGoLeft(i, j), canGoUp(i, j), canGoDown(i, j)] if x]
 
 # read from input and generate board
-for i in range(8):
+for i in range(BOARD_SIZE):
     # add row to board
-    board.append(input().split())
+    rowInput = input().split()
+    for j in range(BOARD_SIZE):
+        char = rowInput[j]
+        if char == 'O':
+            whitePieces.add((i, j))
+        elif char == '@':
+            blackPieces.add((i, j))
 
-# print(board)
+print(whitePieces, blackPieces)
 
 # determine if it's "Moves" or "Massacre")
 command = input()
 
 if command == 'Moves':
-    for i in range(8):
-        for j in range(8):
-            currentchar = board[i][j]
-            if currentchar == 'O':
-                whiteMoves += len(calcMoves(i, j))
-            elif currentchar == '@':
-                blackMoves += len(calcMoves(i, j))
+    for whiteCoord in whitePieces:
+        whiteMoves += len(calcMoves(whiteCoord))
+    for blackCoord in blackPieces:
+        blackMoves += len(calcMoves(blackCoord))
 
     print(whiteMoves)
     print(blackMoves)
