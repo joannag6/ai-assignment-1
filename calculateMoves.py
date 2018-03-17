5ab96d8f1f6edc1627bcacf6d3ae310be76995dd
Joanna <joannag6@gmail.com>
2018-03-17 14:58:34 +1100

    Moved all check moves function into GameState class so they can be called for each specific GameState

--- calculateMoves.py
+++ calculateMoves.py
@@ -1,3 +1,13 @@
+BOARD_SIZE = 8
+
+def corner(i, j):
+    corner_coords = {0, BOARD_SIZE-1}
+    return i in corner_coords and j in corner_coords
+
+def withinBounds(i, j):
+    return (0 <= i < BOARD_SIZE) and (0 <= j < BOARD_SIZE)
+
+
 class GameState:
     #   sets of whitePieces and blackPieces
     #   list of prevMoves, construct from parent
@@ -7,80 +17,75 @@ class GameState:
         self.blackPieces = blackPieces
         self.prevMoves = prevMoves
 
+    def updateSets(self, newWhiteSet, newBlackSet):
+        self.whitePieces = newWhiteSet
+        self.blackPieces = newBlackSet
+
     def isEqual(self, otherGameState):
         return self.whitePieces == otherGameState.whitePieces and self.blackPieces == otherGameState.blackPieces
 
-BOARD_SIZE = 8
-blackPieces = set()
-whitePieces = set()
-whiteMoves = 0
-blackMoves = 0
+    def isEmpty_(self, i, j):
+        return (i, j) not in self.blackPieces and (i, j) not in self.whitePieces and not corner(i, j)
 
-def corner(i, j):
-    corner_coords = {0, BOARD_SIZE-1}
-    return i in corner_coords and j in corner_coords
+    def canJumpRight(self, i, j):
+        return (i,j+2) if withinBounds(i,j+2) and self.isEmpty_(i,j+2) else None
 
-def isEmpty(i, j):
-    return (i, j) not in blackPieces and (i, j) not in whitePieces and not corner(i, j)
+    def canJumpLeft(self, i, j):
+        return (i,j-2) if withinBounds(i,j-2) and self.isEmpty_(i,j-2) else None
 
-def withinBounds(i, j):
-    return (0 <= i < BOARD_SIZE) and (0 <= j < BOARD_SIZE)
+    def canJumpUp(self, i, j):
+        return (i-2,j) if withinBounds(i-2,j) and self.isEmpty_(i-2,j) else None
 
-def canJumpRight(i, j):
-    return (i,j+2) if withinBounds(i,j+2) and isEmpty(i,j+2) else None
+    def canJumpDown(self, i, j):
+        return (i+2,j) if withinBounds(i+2,j) and self.isEmpty_(i+2,j) else None
 
-def canJumpLeft(i, j):
-    return (i,j-2) if withinBounds(i,j-2) and isEmpty(i,j-2) else None
+    def canGoRight(self, i, j):
+        return (i,j+1) if withinBounds(i,j+1) and self.isEmpty_(i,j+1) else self.canJumpRight(i,j)
 
-def canJumpUp(i, j):
-    return (i-2,j) if withinBounds(i-2,j) and isEmpty(i-2,j) else None
+    def canGoLeft(self, i, j):
+        return (i,j-1) if withinBounds(i,j-1) and self.isEmpty_(i,j-1) else self.canJumpLeft(i,j)
 
-def canJumpDown(i, j):
-    return (i+2,j) if withinBounds(i+2,j) and isEmpty(i+2,j) else None
+    def canGoUp(self, i, j):
+        return (i-1,j) if withinBounds(i-1,j) and self.isEmpty_(i-1,j) else self.canJumpUp(i,j)
 
-def canGoRight(i, j):
-    return (i, j+1) if withinBounds(i,j+1) and isEmpty(i,j+1) else canJumpRight(i, j)
+    def canGoDown(self, i, j):
+        return (i+1,j) if withinBounds(i+1,j) and self.isEmpty_(i+1,j) else self.canJumpDown(i,j)
 
-def canGoLeft(i, j):
-    return (i,j-1) if withinBounds(i,j-1) and isEmpty(i,j-1) else canJumpLeft(i, j)
+def setUpBoard():
+    blackPieces = set()
+    whitePieces = set()
+    # read from input and generate board
+    for i in range(BOARD_SIZE):
+        # add row to board
+        rowInput = input().split()
+        for j in range(BOARD_SIZE):
+            char = rowInput[j]
+            if char == 'O':
+                whitePieces.add((i, j))
+            elif char == '@':
+                blackPieces.add((i, j))
 
-def canGoUp(i, j):
-    return (i-1,j) if withinBounds(i-1,j) and isEmpty(i-1,j) else canJumpUp(i, j)
+    startState.updateSets(whitePieces, blackPieces)
 
-def canGoDown(i, j):
-    return (i+1,j) if withinBounds(i+1,j) and isEmpty(i+1,j) else canJumpDown(i, j)
-
-def calcMoves(coord): # TODO(return list of possible moves)
+def calcMovesForCoord(coord): # TODO(return list of possible moves)
     i, j = coord
-    return [x for x in [canGoRight(i, j), canGoLeft(i, j), canGoUp(i, j), canGoDown(i, j)] if x]
-
-# read from input and generate board
-for i in range(BOARD_SIZE):
-    # add row to board
-    rowInput = input().split()
-    for j in range(BOARD_SIZE):
-        char = rowInput[j]
-        if char == 'O':
-            whitePieces.add((i, j))
-        elif char == '@':
-            blackPieces.add((i, j))
-
-# determine if it's "Moves" or "Massacre")
-command = input()
+    return [x for x in [startState.canGoRight(i, j), startState.canGoLeft(i, j), startState.canGoUp(i, j), startState.canGoDown(i, j)] if x]
 
 # Function that calculates the total number of moves, then prints them to stdout
-def calculateTotalMoves(whiteMoves, blackMoves):
+def calcTotalMoves():
+    whiteMoves = 0
+    blackMoves = 0
+
     #TODO(shouldn't need to pass in whiteMoves and blackMoves, might use class next time)
-    for whiteCoord in whitePieces:
-        whiteMoves += len(calcMoves(whiteCoord))
-    for blackCoord in blackPieces:
-        blackMoves += len(calcMoves(blackCoord))
+    for whiteCoord in startState.whitePieces:
+        whiteMoves += len(calcMovesForCoord(whiteCoord))
+    for blackCoord in startState.blackPieces:
+        blackMoves += len(calcMovesForCoord(blackCoord))
 
     print(whiteMoves)
     print(blackMoves)
 
 def massacre():
-
     # set startState
     # moveSequence stack [((x1, y1), (x2, y2))]
     # visited = set(GameStates)
@@ -98,12 +103,18 @@ def massacre():
     # return moveSequence (prettified)
     print('yay massacre')
 
-if command == 'Moves':
-    calculateTotalMoves(whiteMoves, blackMoves)
+startState = GameState(set(), set(), [])
 
+setUpBoard()
+
+# determine if it's "Moves" or "Massacre")
+command = input()
+
+if command == 'Moves':
+    # calculate available moves for each player
+    calcTotalMoves()
 elif command == 'Massacre':
     # do massacre stuff
     massacre()
-
 else:
     print("Please enter a valid command (ie. 'Moves', 'Massacre')")
