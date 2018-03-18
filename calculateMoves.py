b2e4241340947f21ce44eee2125773e1ba93fb9a
Joanna <joannag6@gmail.com>
2018-03-18 23:46:08 +1100

    Got part 1B working with simple DFS. Prints out sequence of moves. This returns a much longer sequence than necessary, but still correct

--- calculateMoves.py
+++ calculateMoves.py
@@ -14,39 +14,60 @@ class Movement:
     # returns list of possible moves
     def calcMovesForCoord(self, coord):
         i, j = coord
-        return [x for x in [self.canGoRight_(i, j), self.canGoLeft_(i, j), self.canGoUp_(i, j), self.canGoDown_(i, j)] if x]
+        moveList = []
+        for move in [self.canGoRight_(i, j),
+                     self.canGoLeft_(i, j),
+                     self.canGoUp_(i, j),
+                     self.canGoDown_(i, j)]:
+            if move:
+                moveList.append(move)
+        return moveList
 
-    # checks if cell is empty, that is it does not contain pieces or is not a corner.
+    # checks if cell is empty, (does not contain pieces and is not a corner)
     def isEmpty_(self, i, j):
-        return (i, j) not in self.state.blackPieces and (i, j) not in self.state.whitePieces and not corner(i, j)
+        return ((i, j) not in self.state.blackPieces
+                and (i, j) not in self.state.whitePieces
+                and not corner(i, j))
 
     # series of functions that checks if a piece can move or jump.
     # canGo functions check if the adjacent cell is empty and within bounds, then tries tso move.
     # if not empty, it is a piece or a corner. We then call canJump functions to see if
     # we can perform a jump, the target cell for jump is empty and within bounds.
     def canJumpRight_(self, i, j):
-        return (i, j + 2) if withinBounds(i, j + 2) and self.isEmpty_(i, j + 2) else None
+        if withinBounds(i, j + 2) and self.isEmpty_(i, j + 2):
+            return (i, j + 2)
 
     def canJumpLeft_(self, i, j):
-        return (i, j - 2) if withinBounds(i, j - 2) and self.isEmpty_(i, j - 2) else None
+        if withinBounds(i, j - 2) and self.isEmpty_(i, j - 2):
+            return (i, j - 2)
 
     def canJumpUp_(self, i, j):
-        return (i - 2, j) if withinBounds(i - 2, j) and self.isEmpty_(i - 2, j) else None
+        if withinBounds(i - 2, j) and self.isEmpty_(i - 2, j):
+            return (i - 2, j)
 
     def canJumpDown_(self, i, j):
-        return (i + 2, j) if withinBounds(i + 2, j) and self.isEmpty_(i + 2, j) else None
+        if withinBounds(i + 2, j) and self.isEmpty_(i + 2, j):
+            return (i + 2, j)
 
     def canGoRight_(self, i, j):
-        return (i, j + 1) if withinBounds(i, j + 1) and self.isEmpty_(i, j + 1) else self.canJumpRight_(i, j)
+        if withinBounds(i, j + 1) and self.isEmpty_(i, j + 1):
+            return (i, j + 1)
+        return self.canJumpRight_(i, j)
 
     def canGoLeft_(self, i, j):
-        return (i, j - 1) if withinBounds(i, j - 1) and self.isEmpty_(i, j - 1) else self.canJumpLeft_(i, j)
+        if withinBounds(i, j - 1) and self.isEmpty_(i, j - 1):
+            return (i, j - 1)
+        return self.canJumpLeft_(i, j)
 
     def canGoUp_(self, i, j):
-        return (i - 1, j) if withinBounds(i - 1, j) and self.isEmpty_(i - 1, j) else self.canJumpUp_(i, j)
+        if withinBounds(i - 1, j) and self.isEmpty_(i - 1, j):
+            return (i - 1, j)
+        return self.canJumpUp_(i, j)
 
     def canGoDown_(self, i, j):
-        return (i + 1, j) if withinBounds(i + 1, j) and self.isEmpty_(i + 1, j) else self.canJumpDown_(i, j)
+        if withinBounds(i + 1, j) and self.isEmpty_(i + 1, j):
+            return (i + 1, j)
+        return self.canJumpDown_(i, j)
 
 
 class GameState:
@@ -55,8 +76,13 @@ class GameState:
 
     def __init__(self, whitePieces, blackPieces, prevMoves):
         self.whitePieces = whitePieces
+        self.whiteSorted = tuple(sorted(whitePieces))
         self.blackPieces = blackPieces
+        self.blackSorted = tuple(sorted(blackPieces))
         self.prevMoves = prevMoves
+
+    def __hash__(self):
+        return hash((self.whiteSorted, self.blackSorted))
 
     def __eq__(self, otherGameState):
         return (self.whitePieces == otherGameState.whitePieces
@@ -64,7 +90,9 @@ class GameState:
 
     def updateSets(self, newWhiteSet, newBlackSet):
         self.whitePieces = newWhiteSet
+        self.whiteSorted = tuple(sorted(newWhiteSet))
         self.blackPieces = newBlackSet
+        self.blackSorted = tuple(sorted(newBlackSet))
 
 #-----------------------------------------------------------------------------
 # CLASS DEFINITIONS END.
@@ -101,8 +129,6 @@ def setUpBoard():
 
 # Function that calculates the total number of moves, then prints them to stdout
 def calcTotalMoves():
-    movementService = Movement(startState)
-
     whiteMoves = 0
     blackMoves = 0
 
@@ -114,6 +140,7 @@ def calcTotalMoves():
     print(whiteMoves)
     print(blackMoves)
 
+
 def debugPrintState(state):
     print("the following is white pieces")
     print(state.whitePieces)
@@ -122,38 +149,34 @@ def debugPrintState(state):
     print("movelist")
     print(state.prevMoves)
 
+
+def printSequence(moveSequence):
+    for (x, y) in moveSequence:
+        print(str(x) + ' -> ' + str(y))
+
+
 def massacre(startState):
-    # debug TODO
-    #debugPrintState(startState)
     numWhite = len(startState.whitePieces)
-    # set startState
-    # visited = set(GameStates)
     visited = {startState}
-    # to_visit stack = [startState]
     to_visit = [startState]
-    # while to_visit:
     while to_visit:
-        #   currentState = to_visit.pop() -- gets the head
         currentState = to_visit.pop()
+
+        # Check if already reached goal state!
         if massacreGoalCheck(currentState):
-            # debugPrintState(currentState)
-            print(currentState.prevMoves)
+            printSequence(currentState.prevMoves)
             return
-        movementService = Movement(currentState)
-        #   add currentState to visitedStates set()
+
+        movementService.updateState(currentState)
         visited.add(currentState)
-        #   generate childStates (by using calcMoves on each whitePiece)
-        # For each whitepiece in the current state...
-        # TODO debug
-        #print("we started while loop")
+
+        # Generate childStates (by using calcMoves on each whitePiece)
         for whitePiece in currentState.whitePieces:
-            # get all moves that piece can do, then for each move...
+            # Get all moves that piece can do, then for each move generate the
+            # GameState node that results from that move with updated white set,
+            # black set and prevMoves list.
             moveList = movementService.calcMovesForCoord(whitePiece)
             for move in moveList:
-                # generate the gamestate node that results from that move...
-                # we need to generate new white set, black set, and update list of prevMoves, then
-                # use those to create new gameState.
-
                 newWhitePieces = currentState.whitePieces.copy()
                 newBlackPieces = currentState.blackPieces.copy()
                 newPrevMoves = currentState.prevMoves.copy()
@@ -164,27 +187,29 @@ def massacre(startState):
 
                 # we then remove eaten black pieces. (white has priority)
                 removeEatenPieces(newBlackPieces, newWhitePieces)
-                # then we remove eaten white piecs.
+                # then we remove eaten white pieces.
                 removeEatenPieces(newWhitePieces, newBlackPieces)
 
                 newGameState = GameState(newWhitePieces, newBlackPieces, newPrevMoves)
 
-                # we then check if number of whitepieces decreased. IF so, we do not add to to_visit set.
-                # we do check if newnode is in visited state, if so we do not add to to_visit set.
-                if (len(newGameState.whitePieces) == numWhite) and newGameState not in visited:
+                # If number of whitepieces decreased, it's not a good move so
+                # don't add to to_visit stack. Also check if newnode has
+                # already been visited state.
+                if (len(newGameState.whitePieces) == numWhite
+                    and newGameState not in visited):
                     to_visit.append(newGameState)
+                    visited.add(newGameState)
 
-    # return moveSequence (prettified)
-    # do this by returning currentState.prevMoves
-
-def isOpponent(opponentPieces, coordinate):
+def isEnemy(enemyPieces, coordinate):
     i, j = coordinate
-    return withinBounds(i, j) and coordinate in opponentPieces or corner(i, j)
+    return withinBounds(i, j) and (coordinate in enemyPieces or corner(i, j))
 
-def canEat(opponentPieces, side1, side2):
-    return isOpponent(opponentPieces, side1) and opponentPieces(opponentPieces, side2)
 
-def removeEatenPieces(ownPieces, opponentPieces):
+def canEat(enemyPieces, side1, side2):
+    return isEnemy(enemyPieces, side1) and isEnemy(enemyPieces, side2)
+
+
+def removeEatenPieces(ownPieces, enemyPieces):
     toRemove = []
     for piece in ownPieces:
         i,j = piece
@@ -195,28 +220,23 @@ def removeEatenPieces(ownPieces, opponentPieces):
 
         # check if piece can be eaten from up and down / left and right
         # by checking if within bounds and if they are corner or white.
-        if canEat(opponentPieces, up, down) or canEat(opponentPieces, left, right):
+        if canEat(enemyPieces, up, down) or canEat(enemyPieces, left, right):
             toRemove.append(piece)
-
     for pieceToRemove in toRemove:
         ownPieces.remove(pieceToRemove)
     return ownPieces
+
 
 # Function to check if we are in a goal state for massacre
 def massacreGoalCheck(gameState):
     # If the state we send has no black pieces, we are in goal state.
     return (len(gameState.blackPieces) == 0)
 
-# Function to check if two states are equivalent in the game.
-def checkEquivalent(state1,state2):
-    return (state1.whitePieces == state2.whitePieces
-            and state1.blackPieces == state2.blackPieces)
-
-
 # FUNCTION DEFINITIONS END
 #-----------------------------------------------------------------------------
 
 startState = GameState(set(), set(), [])
+movementService = Movement(startState)
 
 setUpBoard()
 
